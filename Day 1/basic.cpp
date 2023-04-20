bec8deaf03be86dabd9210a62fae8aac58729e48
ayushhh321 <ayush56039@gmail.com>
2023-04-20 18:27:17 +0530

    basic

---
+++ Day 1/basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+//this is single line comment. and it can be added anywhere
+/*ok
+fax*/
+int main() {
+    cout <<"hello world";
+
+
+    return 0;
+}
+
