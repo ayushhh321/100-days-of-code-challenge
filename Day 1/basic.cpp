eb38cb2fbc7906726ab1cc8782512bb6121b8336
ayushhh321 <ayush56039@gmail.com>
2023-04-21 11:58:15 +0530

    Day 1

--- Day 1/basic.cpp
+++ Day 1/basic.cpp
@@ -41,7 +41,7 @@ int a = 4;
     cin>>num1;   //cin is kinda like mere se lo like num1 ko bola mere me aao 
 
 
-    cout<<"enter the value of num2:\n";
+    cout<<"enter the value of num2:\n";    
     cin>>num2;
 
     cout<<"the sum is:\n"<<num1+num2; 
