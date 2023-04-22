930b3d9cd9532d6fc608533f17a4c77a1666b83f
ayushhh321 <ayush56039@gmail.com>
2023-04-22 23:44:20 +0530

    question hackerrank

---
+++ Day 3/day3.cpp
@@ -0,0 +1,2 @@
+//Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
+
