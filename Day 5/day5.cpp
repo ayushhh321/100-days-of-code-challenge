1cc13184db431f09364daa076f1c3b834d924173
ayushhh321 <ayush56039@gmail.com>
2023-04-24 23:54:30 +0530

    array

---
+++ Day 5/day5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+ int main()
+ {
+    int marks[]={12,13,14,15};
+
+    cout<<marks[0]<<endl;
+    cout<<marks[0]<<endl;
+    cout<<marks[0]<<endl;
+    cout<<marks[0]<<endl;
+    
+    return 0;
+ }
