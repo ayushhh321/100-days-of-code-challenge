7c1f3160a1e01c6215c115e87524df91ca80eaa4
ayushhh321 <ayush56039@gmail.com>
2023-04-27 23:52:09 +0530

    conditional

---
+++ Day 7/day7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n ;
+    cin>>n;
+
+    if(n>0);
+    {
+        cout<<"hellow world";
+        
+    }
+    
+    return 0;
+
+}
