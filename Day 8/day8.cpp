302083723299a2d62246d5cfada517c6c067d2f4
ayushhh321 <ayush56039@gmail.com>
2023-04-28 16:26:52 +0530

    while loop

---
+++ Day 8/day8.cpp
@@ -0,0 +1,21 @@
+//looping L3
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin>>n;
+
+    int i = 1; //bcz ginti 1 se start krni hai n tk jaha tk chaiye humko
+
+    while(i<=n) 
+    {
+        cout<<i<<endl;
+        i=i+1;  // age badhne ke liye add krna hoga
+    }
+    
+    
+    return 0 ;
+}
