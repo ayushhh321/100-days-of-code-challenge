81ddebf72b90b6284fd27e2f6a1efdefafb7d89b
ayushhh321 <ayush56039@gmail.com>
2023-04-28 15:07:42 +0530

    continonal 3

--- Day 7/day7.cpp
+++ Day 7/day7.cpp
@@ -4,18 +4,39 @@ using namespace std;
 int main()
 {
 
-    int a;
-    cin>>a;
+    //int a;
+    // cin>>a;
 
-    if(a>0)
-    {
-      cout<<"A is positive"<<endl;
-    }
+    // if(a>0)
+    // {
+    //   cout<<"A is positive"<<endl;
+    // }
 
-    else
-    {
-      cout<<"A is negative"<<endl;
-    }
+    // else
+    // {
+    //   cout<<"A is negative"<<endl;
+    // }
+
+    // to print and check a>b and b<a 
+
+   int a , b ;
+   cout<<"enter the value of a "<<endl;
+   cin>>a;
+
+   cout<<"enter the value of b "<<endl;
+   cin>>b;
+
+   if(a>b)
+   {
+    cout<<"A is greater"<<endl;
+   }
+
+ if(a<b)
+ {
+    cout<<"B is greater"<<endl;
+ }
+
+
     return 0;
 
 }
