d05a4a6a51b3223818a3f3acf24910af2159aa1c
ayushhh321 <ayush56039@gmail.com>
2023-04-28 15:10:19 +0530

    conditional P 4

--- Day 7/day7.cpp
+++ Day 7/day7.cpp
@@ -19,24 +19,26 @@ int main()
 
     // to print and check a>b and b<a 
 
-   int a , b ;
-   cout<<"enter the value of a "<<endl;
-   cin>>a;
+//    int a , b ;
+//    cout<<"enter the value of a "<<endl;
+//    cin>>a;
 
-   cout<<"enter the value of b "<<endl;
-   cin>>b;
+//    cout<<"enter the value of b "<<endl;
+//    cin>>b;
 
-   if(a>b)
-   {
-    cout<<"A is greater"<<endl;
-   }
+//    if(a>b)
+//    {
+//     cout<<"A is greater"<<endl;
+//    }
 
- if(a<b)
- {
-    cout<<"B is greater"<<endl;
- }
+//  if(a<b)
+//  {
+//     cout<<"B is greater"<<endl;
+//  }
 
+   //print the number is positive , negative or zero
 
+   
     return 0;
 
 }
