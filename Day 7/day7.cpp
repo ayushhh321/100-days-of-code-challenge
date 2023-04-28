5cbfd23a5fc9c8796c762946249f32ff0a98f95b
ayushhh321 <ayush56039@gmail.com>
2023-04-28 15:59:03 +0530

    P3 solved

--- Day 7/day7.cpp
+++ Day 7/day7.cpp
@@ -60,7 +60,26 @@ int main()
 
  //P 3 a---z , A----Z , 0---9 --->> print lower , upper and numeric respeticvely
 
-   
+     char ch;
+     cout<<"enter the character"<<endl;
+     cin>>ch;
+
+     if(ch>='a' && ch<='z')
+     {
+        cout<<"Given charcter is lower case"<<endl;
+     }
+    
+    else if(ch>='0' && ch<='9')
+    {
+        cout<<"given charcter is numeric"<<endl;
+    }
+
+    else
+    {
+        cout<<"given charcter is upper case"<<endl;
+        
+
+    }
 
     return 0;
 
