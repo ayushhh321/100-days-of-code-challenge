07dde0aa3c99cdbb68f3611d122223340e15c096
ayushhh321 <ayush56039@gmail.com>
2023-05-09 20:49:29 +0530

    error removed

--- Day 10/day10.cpp
+++ Day 10/day10.cpp
@@ -8,11 +8,11 @@ int main()
     int a = 4;
     int b = 6;
 
-    cout<<"a&b"<<a&b<<endl;
-    cout<<"a|b"<<a|b<<endl;
-    cout<<"~a"<<~a<<endl;
-    cout<<"a^b"<<a^b<<endl;
+    cout<<"a&b"<<(a&b)<<endl;
+    cout<<"a|b"<<(a|b)<<endl;
+    cout<<"~a"<<(~a)<<endl;
+    cout<<"a^b"<<(a^b)<<endl;
 
    return 0;
-   
+
 }
