2bf94d6fe9a19e0889869bae52f610fdfbee0551
ayushhh321 <ayush56039@gmail.com>
2023-05-09 19:38:08 +0530

    solved prime

--- Day 9/day9.cpp
+++ Day 9/day9.cpp
@@ -9,10 +9,17 @@ int main()
     int n;
     cin>>n;
 
+    int i =2;
 
-
-    
-    
-    
+    while(i<n)
+    {
+        if(n%i==0){
+        cout<<"NOt prime for "<< i<<endl;
+        }
+        else{
+        cout<<"prime for "<< i<<endl;
+        }
+        i=i+1;
+    }
     return 0;
 }
