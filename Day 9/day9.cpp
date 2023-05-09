0e39684536df7aa61df9caf67fe04e2f4e0b5aa7
ayushhh321 <ayush56039@gmail.com>
2023-05-09 19:25:59 +0530

    question added

--- Day 9/day9.cpp
+++ Day 9/day9.cpp
@@ -1,12 +1,16 @@
 //Looping question l3
 
+//check weather given number is prime or not
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    
-    
+    int n;
+    cin>>n;
+
+
+
     
     
     
