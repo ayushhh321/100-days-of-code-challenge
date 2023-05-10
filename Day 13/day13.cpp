c59f7a3cdd0eb4034acc5b2442e9ba33bf9aec7b
ayushhh321 <ayush56039@gmail.com>
2023-05-10 23:29:19 +0530

    A ADDED

--- Day 13/day13.cpp
+++ Day 13/day13.cpp
@@ -6,7 +6,15 @@ using namespace std;
 
 int main()
 {
-    
+    int n ;
+    cout<<"enter the no."<<endl;
+    cin>>n;
+
+    for(int i =1 ; i<=n ; i++)
+    {
+        cout<<i<<endl;
+    }
+
     
     
     
