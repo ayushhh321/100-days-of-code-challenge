5720f0c3e9f63682b444d175cb7a8af663673c19
ayushhh321 <ayush56039@gmail.com>
2023-05-10 20:08:01 +0530

    solved

--- Day 12/day12.cpp
+++ Day 12/day12.cpp
@@ -6,7 +6,7 @@ int main()
     int a = 1;
     int b =2;
 
-    if(a-- >0 && ++b>2)
+    if(a-- >0 && ++b>2)//true and true hora mns 1&&1 toh using AND it is 1 i.e true
     {
         cout<<"stage 1 "<<endl;
     }
