cfe03bf8c67cf574ea13bbd4c06540bb4adb9ba6
ayushhh321 <ayush56039@gmail.com>
2023-05-10 23:27:22 +0530

    BP added

--- Day 13/day13.cpp
+++ Day 13/day13.cpp
@@ -1,2 +1,25 @@
 //for loop
 //print 1 to n using for loop
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
