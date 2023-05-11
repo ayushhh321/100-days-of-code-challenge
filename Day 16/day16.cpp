9e2c4324c5f8b177bcfbb5b601fff7949687189d
ayushhh321 <ayush56039@gmail.com>
2023-05-11 15:51:11 +0530

    test case 1 passed

--- Day 16/day16.cpp
+++ Day 16/day16.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cout<<"enter the value of n "<<endl;
+    cin>>n;
+
+    for(int i =2 ; i<n ; i++)
+    {
+        if(n%i==0)
+        {
+            cout<<"not prime for "<<i<<endl;
+
+        }
+        
+        else{
+            cout<<"prime for "<<i<<endl;
+        }
+    }
     
     
     
