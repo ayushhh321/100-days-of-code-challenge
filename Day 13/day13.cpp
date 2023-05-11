3eabc74cec2a19487513436504fbb49f2718f95b
ayushhh321 <ayush56039@gmail.com>
2023-05-11 11:22:25 +0530

    in

--- Day 13/day13.cpp
+++ Day 13/day13.cpp
@@ -19,7 +19,11 @@ int main()
     
     
     //multiples condition in For loop
-    
+
+    for(int a=0,b=1,c=2; a<=0 , b<=1 , c<=2 ; a++ , b++ , c++)
+    {
+        cout<<a<<""<<b<<""<<c<<endl;
+    }
     
     
     
