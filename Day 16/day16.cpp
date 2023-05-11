4072c3df2b513c3a69c8bba698c83aefdafc3b14
ayushhh321 <ayush56039@gmail.com>
2023-05-11 15:45:46 +0530

    BP added

--- Day 16/day16.cpp
+++ Day 16/day16.cpp
@@ -1,1 +1,19 @@
 //chcek weather the given no. is prime or not using for loop  
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
