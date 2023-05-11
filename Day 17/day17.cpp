e4547851ea682b9d7822f7b6bf52d07c04e8135f
ayushhh321 <ayush56039@gmail.com>
2023-05-11 19:26:17 +0530

    exe

--- Day 17/day17.cpp
+++ Day 17/day17.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main()
 {
-    for(int i =0 ; i<=1 ; i++)
-    {
-        cout<<"hi"<<endl;
-        i=0;
-    }
+    for (int i = 1; i <= 10; i++)
+     {
+        for (int j = 1; j <= 10; j++) {
+        cout << i * j << "\t";
+   }
+   cout << endl;
+}
+
 }
