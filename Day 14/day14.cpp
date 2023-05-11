9e8bb68e3c919af5b5271b5085264b51ceaf63e0
ayushhh321 <ayush56039@gmail.com>
2023-05-11 12:05:34 +0530

    added aw

--- Day 14/day14.cpp
+++ Day 14/day14.cpp
@@ -6,8 +6,20 @@ using namespace std;
 int main()
 {
     
+    int n;
+    cout<<"enter the vlaue"<<endl;
+    cin>>n;
+
+   int sum=0;
+
+   for(int i=1 ; i<=n ;i++)
+   {
+    sum=sum+i;
+
     
+   }
     
+    cout<<sum<<endl;
     
     
     
