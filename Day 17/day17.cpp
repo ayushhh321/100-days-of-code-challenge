579332e1861a48735d6342caa5e99b2cf18a4305
ayushhh321 <ayush56039@gmail.com>
2023-05-13 04:25:22 +0530

    scope

--- Day 17/day17.cpp
+++ Day 17/day17.cpp
@@ -1,15 +1,1 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    
-    for (int i = 1; i <=10; i++)
-    {
-        cout<<"* ";
-        
-    }
-     
-        
-       return 0;
-}
+//scope and operator precedence
