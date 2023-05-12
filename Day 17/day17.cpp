a7974dd006daa01e1961423a0e8da382fee622c9
ayushhh321 <ayush56039@gmail.com>
2023-05-13 04:30:13 +0530

    5 cases

--- Day 17/day17.cpp
+++ Day 17/day17.cpp
@@ -5,8 +5,27 @@ using namespace std;
 
 int main()
 {
+    int q;
+    cout<<q<<endl;//it will print any value garbage value of Q bcz q is undefined
+ 
     
+    int a =3;
+    cout<<a<<endl;
+
+    if(true)
+    {
+        int b=5;
+        cout<<b<<endl;//b=5 will be printed bcz it is into its block
+
+    }
+
+    int b=1;
+    cout<<b<<endl;//will print b=1
+
+    int b=3;
+    cout<<b<<endl;//it will now create error like same block two diffrent defined values
     
+        
     
     
     
