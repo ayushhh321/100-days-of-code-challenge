d59595690254753c096e7ef6c4bd0d4be5a3fdcf
ayushhh321 <ayush56039@gmail.com>
2023-05-13 04:44:27 +0530

    OP added

--- Day 17/day17.cpp
+++ Day 17/day17.cpp
@@ -42,7 +42,11 @@ int main()
         }
     } 
     
-    
-    
+//     //Operator precedence in C++ determines the order in which different operators in an expression are evaluated. Some operators have higher precedence than others, which means they are evaluated first before the lower precedence operators. This can affect the outcome of an expression if the operators are not evaluated in the correct order.
+
+// For example, in the expression 4 + 5 * 3, the multiplication operator * has higher precedence than the addition operator +, so it is evaluated first. Therefore, the expression is evaluated as 4 + (5 * 3), which results in 19.
+
+// Knowing the operator precedence rules is important when writing complex expressions to ensure they are evaluated correctly. Parentheses can also be used to modify the precedence order of operators in an expression
+
     return 0;
 }
