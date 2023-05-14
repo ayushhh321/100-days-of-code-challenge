e19109929d9a314703f7847f0cd980657664af9f
ayushhh321 <ayush56039@gmail.com>
2023-05-14 17:52:37 +0530

    prob 3 added

--- test question file.cpp
+++ test question file.cpp
@@ -40,47 +40,51 @@
 
 
 //problem 2 - 1-9 write in lowercase and for greater than 9 print greater than 9
-#include<iostream>
-using namespace std ;
+// #include<iostream>
+// using namespace std ;
 
-int main()
-{
-	int n;
-	cout<<"write your number"<<endl;
-	cin>>n;
+// int main()
+// {
+// 	int n;
+// 	cout<<"write your number"<<endl;
+// 	cin>>n;
 
-	 if(n==1)
-	 {
-		cout<<"one"<<endl;
-	 }
-	 else if(n==2)
-	 {cout<<"two"<<endl;}
+// 	 if(n==1)
+// 	 {
+// 		cout<<"one"<<endl;
+// 	 }
+// 	 else if(n==2)
+// 	 {cout<<"two"<<endl;}
      
-	 else if(n==3)
-	 {cout<<"three"<<endl;}
+// 	 else if(n==3)
+// 	 {cout<<"three"<<endl;}
 
-	 else if(n==4)
-	 {cout<<"four"<<endl;}
+// 	 else if(n==4)
+// 	 {cout<<"four"<<endl;}
 
-	 else if(n==5)
-	 {cout<<"five"<<endl;}
+// 	 else if(n==5)
+// 	 {cout<<"five"<<endl;}
 
-	 else if(n==6)
-	 {cout<<"six"<<endl;}
+// 	 else if(n==6)
+// 	 {cout<<"six"<<endl;}
 
-	 else if(n==7)
-	 {cout<<"seven"<<endl;}
+// 	 else if(n==7)
+// 	 {cout<<"seven"<<endl;}
 
-	 else if(n==8)
-	 {cout<<"eight"<<endl;}
+// 	 else if(n==8)
+// 	 {cout<<"eight"<<endl;}
 
-	 else if(n==9)
-	 {cout<<"nine"<<endl;}
+// 	 else if(n==9)
+// 	 {cout<<"nine"<<endl;}
      
-     else
-	 {
-		cout<<"Greater than 9"<<endl;
-	 }
+//      else
+// 	 {
+// 		cout<<"Greater than 9"<<endl;
+// 	 }
 
-    return 0;
+//     return 0;
+
+//print the gratest of input numbers
+
+
 }
