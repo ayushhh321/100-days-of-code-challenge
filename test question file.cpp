dbd35de109e3cddc6169466dabfb8d56f2c23a67
ayushhh321 <ayush56039@gmail.com>
2023-05-14 17:33:59 +0530

    part 1 solved

--- test question file.cpp
+++ test question file.cpp
@@ -44,10 +44,43 @@ int main()
 	cout<<"write your number"<<endl;
 	cin>>n;
 
-	 if(n==1,2,3,4,5,6,7,8,9)
+	 if(n==1)
 	 {
-		cout<<"one,two,three,four,five,six,seven,eight,nine"<<endl;
-
+		cout<<"one"<<endl;
 	 }
+	 else if(n==2)
+	 {cout<<"two"<<endl;}
+     
+	 else if(n==3)
+	 {cout<<"three"<<endl;}
+
+	 else if(n==4)
+	 {cout<<"four"<<endl;}
+
+	 else if(n==5)
+	 {cout<<"five"<<endl;}
+
+	 else if(n==6)
+	 {cout<<"six"<<endl;}
+
+	 else if(n==7)
+	 {cout<<"seven"<<endl;}
+
+	 else if(n==8)
+	 {cout<<"eight"<<endl;}
+
+	 else if(n==9)
+	 {cout<<"nine"<<endl;}
+     
+
+	 
+
+
+
+
+
+
+
+
 	 return 0;
 }
