b65b50f7905a86c9871d7495e08a235b58fe5ea4
ayushhh321 <ayush56039@gmail.com>
2023-06-14 17:29:41 +0530

    updation

---
+++ Day 19/day19.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "enter ur number" << endl;
+    cin >> n;
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (n % 2 == 0)
+        {
+            cout << "Given number is even" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Given number is odd" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
