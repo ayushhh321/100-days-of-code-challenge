c13c41925c3ed81b70a28a536a90aa46bc8aaa91
ayushhh321 <ayush56039@gmail.com>
2023-06-16 11:34:22 +0530

    expl 2

--- Day 19/day19.cpp
+++ Day 19/day19.cpp
@@ -11,8 +11,11 @@ int main()
     cin >> n;
 
 
-//here we can see we dont need loop for chceking even odd 
+//here we can see we dont need loop for chceking even odd
+
     // for (int i = 0; i <= n; i++)
+
+    //to handle for loop in conditional statement we have to use break and continue statement also .
     {
         if (n % 2 == 0)
         {
