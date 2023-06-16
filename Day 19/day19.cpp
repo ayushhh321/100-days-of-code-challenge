90bd53489598cea9263a1dd0c2e2ec2232a5b67d
ayushhh321 <ayush56039@gmail.com>
2023-06-16 11:31:50 +0530

    loop chcek

--- Day 19/day19.cpp
+++ Day 19/day19.cpp
@@ -1,3 +1,5 @@
+// write code to chc
+
 #include <iostream>
 using namespace std;
 
@@ -8,17 +10,17 @@ int main()
     cout << "enter ur number" << endl;
     cin >> n;
 
-    for (int i = 0; i <= n; i++)
+    // for (int i = 0; i <= n; i++)
     {
         if (n % 2 == 0)
         {
             cout << "Given number is even" << endl;
-            break;
+            // break;
         }
         else
         {
             cout << "Given number is odd" << endl;
-            break;
+            // break;
         }
     }
 
