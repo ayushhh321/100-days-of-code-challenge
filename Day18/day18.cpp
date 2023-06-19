788d7c200207247b208e2269e13dfe28568a5451
ayushhh321 <ayush56039@gmail.com>
2023-06-19 17:05:58 +0530

    exe 18

--- Day18/day18.cpp
+++ Day18/day18.cpp
@@ -83,3 +83,30 @@
 //     return 0;
 
 
+// chcek weather the given no. is prime or not using for loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // method 1 isase kya hoga ki i ki saari value n tk pata chal jyegi kisase prime hai kisase nai hai
+    int n;
+    cout << "enter the value of n " << endl;
+    cin >> n;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "not prime"<< endl;
+            break;
+        }
+
+        else
+        {
+            cout << "prime for" << endl;
+            break;
+        }
+    }
+}
