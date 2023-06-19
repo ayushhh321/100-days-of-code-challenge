2b41ab714c1d40f6e4090a3714483edad3a2a675
ayushhh321 <ayush56039@gmail.com>
2023-06-19 17:16:59 +0530

    exe and temop

--- Day 9/day9.cpp
+++ Day 9/day9.cpp
@@ -23,3 +23,12 @@ int main()
     }
     return 0;
 }
+
+
+The given code snippet has a time complexity of O(log n), where n is the input value.
+
+The loop iterates as long as the condition "i <= n" holds true. The variable "i" is multiplied by 2 in each iteration, starting from an initial value of 1.
+
+Let's consider the number of iterations required to reach or exceed the value of n. At each iteration, "i" is doubled, so the loop will execute log₂(n) times before "i" exceeds n.
+
+Therefore, the time complexity of the code is O(log n).
