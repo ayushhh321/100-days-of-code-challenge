2450b43638126c8284a978487f85d67d85099c0c
ayushhh321 <ayush56039@gmail.com>
2023-06-22 18:41:45 +0530

    vusion sobel

--- vision.py
+++ vision.py
@@ -44,10 +44,18 @@ image = cv2.imread('121171253.jpeg', 0)  # Replace 'image.jpg' with your image f
 # Apply Canny edge detection
 edges_canny = cv2.Canny(image, 150, 150)  # Adjust the threshold values (100 and 200) as needed
 
-# Apply Sobel edge detection
-sobel_x = cv2.Sobel(image, cv2.CV_8U, 1, 0, ksize=3)
-sobel_y = cv2.Sobel(image, cv2.CV_8U, 0, 1, ksize=3)
-edges_sobel = cv2.magnitude(sobel_x, sobel_y)
+# # Apply Sobel edge detection
+# sobel_x = cv2.Sobel(image, cv2.CV_8U, 1, 0, ksize=3)
+# sobel_y = cv2.Sobel(image, cv2.CV_8U, 0, 1, ksize=3)
+# edges_sobel = cv2.magnitude(sobel_x, sobel_y)
+
+image =cv2.imread("21BCE10374_Photo.JPG",0)# reading the image
+sobelx = cv2.Sobel(src=image,ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
+sobely = cv2.Sobel(src=image,ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
+sobelxy = cv2.Sobel(src=image,ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+cv2.imshow('Original Image', image)
+cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
+cv2.waitKey(0)
 
 # Display the original image, Canny edges, and Sobel edges
 cv2.imshow('Original Image', image)
@@ -57,3 +65,12 @@ cv2.imshow('Sobel Edges', edges_sobel)
 # Wait for a key press and then close the windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+import cv2
+image =cv2.imread("21BCE10374_Photo.JPG",0)# reading the image
+sobelx = cv2.Sobel(src=image,ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
+sobely = cv2.Sobel(src=image,ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
+sobelxy = cv2.Sobel(src=image,ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+cv2.imshow('Original Image', image)
+cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
+cv2.waitKey(0)
