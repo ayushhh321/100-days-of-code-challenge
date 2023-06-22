51fd0261507dacc4cf7009d1be2831a78c93344d
ayushhh321 <ayush56039@gmail.com>
2023-06-22 13:26:42 +0530

    .py vision

--- vision.py
+++ vision.py
@@ -2,10 +2,12 @@ import cv2
 # Load the image
 image = cv2.imread('121171253.jpeg', 0)  # Replace 'image.jpg' with your image file
 # Apply Canny edge detection
-edges = cv2.Canny(image, 100, 200)  # Adjust the threshold values (100 and 200) as needed
+edges = cv2.Canny(image, 150, 150)  # Adjust the threshold values (100 and 200) as needed
 # Display the original image and the detected edges
 cv2.imshow('Original Image', image)
 cv2.imshow('Edges', edges)
 # Wait for a key press and then close the windows
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
