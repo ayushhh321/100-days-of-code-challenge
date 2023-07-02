61781c65ad01c5a550add1dbbc07db7abcce2c60
ayushhh321 <ayush56039@gmail.com>
2023-07-02 23:07:37 +0530

    question

--- Day 19/day19.cpp
+++ Day 19/day19.cpp
@@ -32,6 +32,18 @@ int main()
     return 0;
 }
 
+//question -- Smallest Even Multiple
 
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n%2==0){
+            return n;
+        }
+        else{
+            return 2*n;
+        }
+    }
+};
 
 
