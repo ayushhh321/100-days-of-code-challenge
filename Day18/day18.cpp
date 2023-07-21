d1f8ecf9ab27d2506e4d102e54487a2e63d560df
ayushhh321 <ayush56039@gmail.com>
2023-07-21 20:21:48 +0530

    paragram question

--- Day18/day18.cpp
+++ Day18/day18.cpp
@@ -1,112 +1,129 @@
-// // problem 1 - 1-9 print in lowercase and apart from this write the even odd
+// // // problem 1 - 1-9 print in lowercase and apart from this write the even odd
 
-// #include <cmath>
-// #include <cstdio>
-// #include <vector>
+// // #include <cmath>
+// // #include <cstdio>
+// // #include <vector>
+// // #include <iostream>
+// // #include <algorithm>
+// // using namespace std;
+
+// // /*For Loop Hackerrank Solution in C++*/
+
+// // int main()
+// // {
+// // 	int num1, num2;
+// // 	cin >> num1 >> num2;
+// // 	string Arr1[9] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+// // 	for (int i = num1; i <= num2; i++)
+// // 	{
+// // 		if (i <= 9)
+// // 		{
+// // 			cout << Arr1[i - 1] << endl;
+// // 		}
+// // 		else
+// // 		{
+// // 			if (i % 2 == 0)
+// // 			{
+// // 				cout << "even" << endl;   
+// // 			}
+// // 			else
+// // 			{
+// // 				cout << "odd" << endl;
+// // 			}
+// // 		}
+// // 	}
+
+// // 	return 0;
+// // }
+
+// // // problem 2 - 1-9 write in lowercase and for greater than 9 print greater than 9
+// //  #include<iostream>
+// //  using namespace std ;
+
+// // int main()
+// // {
+// // 	int n;
+// // 	cout<<"write your number"<<endl;
+// // 	cin>>n;
+
+// // 	 if(n==1)
+// // 	 {
+// // 		cout<<"one"<<endl;
+// // 	 }
+// // 	 else if(n==2)
+// // 	 {cout<<"two"<<endl;}
+
+// // 	 else if(n==3)
+// // 	 {cout<<"three"<<endl;}
+
+// // 	 else if(n==4)
+// // 	 {cout<<"four"<<endl;}
+
+// // 	 else if(n==5)
+// // 	 {cout<<"five"<<endl;}
+
+// // 	 else if(n==6)
+// // 	 {cout<<"six"<<endl;}
+
+// // 	 else if(n==7)
+// // 	 {cout<<"seven"<<endl;}
+
+// // 	 else if(n==8)
+// // 	 {cout<<"eight"<<endl;}
+
+// // 	 else if(n==9)
+// // 	 {cout<<"nine"<<endl;}
+
+// //      else
+// // 	 {
+// // 		cout<<"Greater than 9"<<endl;
+// // 	 }
+
+// //     return 0;
+
+
+// // chcek weather the given no. is prime or not using for loop
+
 // #include <iostream>
-// #include <algorithm>
 // using namespace std;
 
-// /*For Loop Hackerrank Solution in C++*/
-
 // int main()
 // {
-// 	int num1, num2;
-// 	cin >> num1 >> num2;
-// 	string Arr1[9] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+//     // method 1 isase kya hoga ki i ki saari value n tk pata chal jyegi kisase prime hai kisase nai hai
+//     int n;
+//     cout << "enter the value of n " << endl;
+//     cin >> n;
 
-// 	for (int i = num1; i <= num2; i++)
-// 	{
-// 		if (i <= 9)
-// 		{
-// 			cout << Arr1[i - 1] << endl;
-// 		}
-// 		else
-// 		{
-// 			if (i % 2 == 0)
-// 			{
-// 				cout << "even" << endl;   
-// 			}
-// 			else
-// 			{
-// 				cout << "odd" << endl;
-// 			}
-// 		}
-// 	}
+//     for (int i = 2; i < n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             cout << "not prime"<< endl;
+//             break;
+//         }
 
-// 	return 0;
+//         else
+//         {
+//             cout << "prime for" << endl;
+//             break;
+//         }
+//     }
 // }
 
-// // problem 2 - 1-9 write in lowercase and for greater than 9 print greater than 9
-//  #include<iostream>
-//  using namespace std ;
 
-// int main()
-// {
-// 	int n;
-// 	cout<<"write your number"<<endl;
-// 	cin>>n;
-
-// 	 if(n==1)
-// 	 {
-// 		cout<<"one"<<endl;
-// 	 }
-// 	 else if(n==2)
-// 	 {cout<<"two"<<endl;}
-
-// 	 else if(n==3)
-// 	 {cout<<"three"<<endl;}
-
-// 	 else if(n==4)
-// 	 {cout<<"four"<<endl;}
-
-// 	 else if(n==5)
-// 	 {cout<<"five"<<endl;}
-
-// 	 else if(n==6)
-// 	 {cout<<"six"<<endl;}
-
-// 	 else if(n==7)
-// 	 {cout<<"seven"<<endl;}
-
-// 	 else if(n==8)
-// 	 {cout<<"eight"<<endl;}
-
-// 	 else if(n==9)
-// 	 {cout<<"nine"<<endl;}
-
-//      else
-// 	 {
-// 		cout<<"Greater than 9"<<endl;
-// 	 }
-
-//     return 0;
-
-
-// chcek weather the given no. is prime or not using for loop
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    // method 1 isase kya hoga ki i ki saari value n tk pata chal jyegi kisase prime hai kisase nai hai
-    int n;
-    cout << "enter the value of n " << endl;
-    cin >> n;
 
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
+string sentence
+       {
+        if (sentence.size()==26)
         {
-            cout << "not prime"<< endl;
-            break;
+            return true
         }
-
-        else
-        {
-            cout << "prime for" << endl;
-            break;
-        }
+        else{
+           return false
     }
-}
+    
+};
