77d376d81dd30565c02a44fc5db76d8426efbdbf
ayushhh321 <ayush56039@gmail.com>
2023-09-17 16:20:41 +0530

    error part

--- Day 21/Day 21.cpp
+++ Day 21/Day 21.cpp
@@ -16,30 +16,32 @@ int main()
     //check();
 
     // concatenation using append
-    string firstName = "Harry ";
-    string lastName = "Bhai";
-    string fullName = firstName.append(lastName);
-    cout << fullName;
+//     string firstName = "Harry ";
+//     string lastName = "Bhai";
+//     string fullName = firstName.append(lastName);
+//     cout << fullName;
 
-  /*C++ uses the + operator for both addition and concatenation.
-    Numbers are added. Strings are concatenated.*/
+//   /*C++ uses the + operator for both addition and concatenation.
+//     Numbers are added. Strings are concatenated.*/
 
-    //int using + for addition
+//     //int using + for addition
 
-    int x = 10;
-    int y = 20;
+//     int x = 10;
+//     int y = 20;
 
-    int z = x+y;
+//     int z = x+y;
 
-    // + used for string which results in concatenation
+//     // + used for string which results in concatenation
 
-    string x = "10";
-    string y = "20";
+//     string x = "10";
+//     string y = "20";
 
-    string z = x + y;
+//     string z = x + y;
 
 
-    
+string x = "10";
+int y = 20;
+string z = x + y;
     
     return 0;
 }
