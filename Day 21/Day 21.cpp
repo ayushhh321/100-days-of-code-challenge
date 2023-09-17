68d686ff83487472300a343d14ed0264a5a28810
ayushhh321 <ayush56039@gmail.com>
2023-09-17 15:33:48 +0530

    Day 21

---
+++ Day 21/Day 21.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string firstName = "Harry ";
+string lastName = "Bhai";
+string fullName = firstName.append(lastName);
+cout << fullName;
+
+
+
+    return 0;
+}
