5c4a990d7458a48cbbf60e14369323f0cfec22f4
ayushhh321 <ayush56039@gmail.com>
2023-11-06 19:15:12 +0530

    max and min ficntion done

--- Day 5/max and min in array.cpp
+++ Day 5/max and min in array.cpp
@@ -3,13 +3,39 @@
 #include<iostream>
 using namespace std;
 
+//as we know int has range of -2^31 to 2^31
+
+int getMax(int arr[], int size)
+{
+  int max = INT_MIN;  //phle hi maan lo max min sare value se Equal hai
+  for(int i=0 ; i<size ; i++){
+    if(arr[i]>max){
+      max=arr[i];
+    }
+  }
+  return max;
+}
+
+int getMin(int arr[], int size)
+{
+  int min=INT_MAX; //phle hi maan lo min max ke saare value se equal hai
+  for(int i=0 ; i<size; i++)
+  {
+    if(arr[i]<min){
+      min=arr[i];
+    }
+  }
+  return min;
+}
+
 int main()
 {
-
-
-
-
-
   
+  
+
+
+
+
+
   return 0;
 }
