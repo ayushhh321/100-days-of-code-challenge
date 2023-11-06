e9cc9d4b3f27fd4a58e368f3e9e4a9f103771c2a
ayushhh321 <ayush56039@gmail.com>
2023-11-06 19:09:27 +0530

    boiler plate

--- Day 5/max and min in array.cpp
+++ Day 5/max and min in array.cpp
@@ -1,2 +1,15 @@
 //this can be also done using pre defined function max() and min() for that you have to include <cmath> libraray
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+  
+  return 0;
+}
