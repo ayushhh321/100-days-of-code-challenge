2bef7211d349b31c68130586073565a91b692d38
ayushhh321 <ayush56039@gmail.com>
2023-11-06 18:02:21 +0530

    phase 2

--- Day 5/array_intro.cpp
+++ Day 5/array_intro.cpp
@@ -1,51 +1,54 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main()
-{
+// int main()
+// {
     
-   //declare
-   int number[15];
+//    //declare
+//    int number[15];
 
-  //accessing an array
-  cout<<"element at number 10 will be some garbage value "<<number[10]<<endl;
-  cout<<"element at number 11 will be some garbage value "<<number[11]<<endl;
+//   //accessing an array
+//   cout<<"element at number 10 will be some garbage value "<<number[10]<<endl;
+//   cout<<"element at number 11 will be some garbage value "<<number[11]<<endl;
 
-//initialising an array
-int second[3]={5,7,11};
+// //initialising an array
+// int second[3]={5,7,11};
 
-//accessing an array
-cout<<"value at 2 index "<<second[2]<<endl;
+// //accessing an array
+// cout<<"value at 2 index "<<second[2]<<endl;
 
-//printing an array of 15 size but only two elements
-int third[15]={2,7};
+// //printing an array of 15 size but only two elements
+// int third[15]={2,7};
 
-int n =15;//size of array
-for(int i=0; i<n ; i++)
-{
-  cout<<third[i]<<endl;
-}
+// int n =15;//size of array
+// for(int i=0; i<n ; i++)
+// {
+//   cout<<third[i]<<endl;
+// }
 
-//printing array with only one elemenet or initialsing all locations with o
+// //printing array with only one elemenet or initialsing all locations with o
 
-int fourth[10]={0};
+// int fourth[10]={0};
 
-int n=10; //size of array i mean kaha tk print krna hai loop kaha tk jyega
+// int n=10; //size of array i mean kaha tk print krna hai loop kaha tk jyega
 
-for(int i=0; i<n ; i++)
-{
-  cout<<fourth[i]<<endl;
-}
+// for(int i=0; i<n ; i++)
+// {
+//   cout<<fourth[i]<<endl;
+// }
 
-//initalising all locations with 1 [not possible with below line use fill_n for that]
-int fifth[10]={1};
+// //initalising all locations with 1 [not possible with below line use fill_n for that]
+// int fifth[10]={1};
 
-int n=10; //size of array
+// int n=10; //size of array
 
-for(int i=0 ; i<n ; i++)
-{
-  cout<<fifth[i]<<endl;
-}
+// for(int i=0 ; i<n ; i++)
+// {
+//   cout<<fifth[i]<<endl;
+// }
 
-  return 0;
-}
+//   return 0;
+// }
+
+//now writinh the wholw code using functions
+
